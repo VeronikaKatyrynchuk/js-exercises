0795b0da8ca1adf745a0c65e34a32066ba573a11
Veronika <nesteryuk15@gmail.com>
2020-08-23 18:04:05 +0300

    2 exrc done

---
+++ src/script.js
@@ -0,0 +1,46 @@
+'use strict'
+
+//first exr is to swap two variables
+
+let a = 17;
+let b = 74;
+[a, b] = [b, a]
+console.log(a);
+console.log(b);//the way by applying destructuring assigment
+
+//second exr 
+let lang = 'ru';
+if(lang == 'ru') {
+    let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+};
+
+if(lang == 'en') {
+     arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+}
+console.log(arr);
+// by using 2if conditional statement
+
+
+let lang2 = 'ru';
+switch(lang2) {
+    case 'ru': 
+        let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+        break;
+
+    case 'en': 
+        arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+        break;
+}
+console.log(arr);
+//by using switch statement
+
+let lang3 = 'ru';
+let arr = {
+    'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+    'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+}
+alert(arr[lang3])
+// by using multidimensional array(многосерный массив)
+
+
+
